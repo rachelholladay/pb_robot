6145df68206c2adb62ad21e2303e2d95e8b523cf
Nishanth Kumar <nishanth.kumar20@gmail.com>
2024-02-23 15:38:07 -0500

    updates to setup and repo structure

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ path_to_myproject = "src/"
 setup(
     name="pbrspot",
     version="0.1.0",
-    packages=find_packages(include=["src", "src/."]),
+    packages=find_packages(include=["pbrspot", "pbrspot.*"]),
     include_package_data=True,
     install_requires=[
         "numpy",
